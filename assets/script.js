372c3e64c8746c48d466f725d46e7daac8f90301
C-dub <25037768+ColeS82@users.noreply.github.com>
2022-11-09 19:01:54 -0700

    day generating.  Safe ZOne

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,51 @@
-const searched= document.getElementById("city-search");
+const now = new Date();
+console.log(now)
+const searched = document.getElementById("city-search");
 const cityName = document.getElementById("city");
-const requestUrl = 'api.openweathermap.org/data/2.5/forecast?q=' + searched + '&appid=a3f3af924ab3526ef547b4a14eefdfc8&units=imperial'
-
 const $searchBtn = document.getElementById('btn');
-$searchBtn.addEventListener('click', getCity);
 
-
-function getCity(event){
-    cityName.innerHTML = "Here's current weather in " + searched.value + ':';
+function getDays(){
+    const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    const dayNumber = now.getDay()
+    
+    console.log(dayNumber)
+    for (i=0 ; i < 6; i++){
+        
+        document.getElementById("wday" + (i + 1)).innerHTML = days[(dayNumber + i) % days.length]
+        
+        console.log(days[(dayNumber + i) % days.length])
+    }}
+    getDays()
+    
+    
+    $searchBtn.addEventListener('click', getCity);
+    function getCity(event) {
+    const city = searched.value
+    cityName.innerHTML = "Here's current weather in " + city + ':';
+    const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=a3f3af924ab3526ef547b4a14eefdfc8&units=imperial`
     event.preventDefault();
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (data) {
+
+            //Getting the min and max values for each day
+            for (i = 0; i < 5; i++) {
+                document.getElementById("day" + (i + 1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min ).toFixed(1) + "°";
+                //Number(1.3450001).toFixed(2); // 1.35
+            };
+
+            for (i = 0; i < 5; i++) {
+                document.getElementById("day" + (i + 1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max ).toFixed(2) + "°";
+            };
+
+            //Getting Weather Icons
+            // for (i = 0; i < 5; i++) {
+            //     document.getElementById("img" + (i + 1)).innerHTML.src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
+            // };
+        })
 }
 
-
-
-// fetch(requestUrl)
-// .then(response => response.json())
-// .then(data =>)
