d37bee6d8be4d4aa58570ba476f9c3a8a2b33de7
C-dub <25037768+ColeS82@users.noreply.github.com>
2022-11-10 01:22:38 -0700

    Revert "Generating p elements in current-weather with unique ids from js"
    This reverts commit 1165744329ae54c76013e267766cc4517049e93a.

--- assets/script.js
+++ assets/script.js
@@ -4,41 +4,26 @@ const searched = document.getElementById("city-search");
 const cityName = document.getElementById("city");
 const $searchBtn = document.getElementById('btn');
 
-//gets days 
 function getDays(){
     const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-
-    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov','Dec'];
-
-    const day = now.getDay();
-    const month = now.getUTCMonth()
-    const date = now.getUTCDate() +'/' + months[month] + '/' + now.getUTCFullYear();
-
+    const dayNumber = now.getDay()
     
-    console.log(day)
-    //itterates the days of the week starting from the current day and places them in the appropriate elements.
+    console.log(dayNumber)
     for (i=0 ; i < 6; i++){
-        document.getElementById('wday' + (i + 1)).innerHTML = days[(day + i) % days.length]
-        console.log(days[(day + i) % days.length])
-    }
-    //Creates p element in current-weather
-    const p = document.createElement('p');
-    p.setAttribute('id', 'date');
-    document.getElementById('current-weather').appendChild(p).innerHTML = date
-
-}
+        
+        document.getElementById("wday" + (i + 1)).innerHTML = days[(dayNumber + i) % days.length]
+        
+        console.log(days[(dayNumber + i) % days.length])
+    }}
     getDays()
     
-    //adds event listener to search button initiating the getCity function
+    
     $searchBtn.addEventListener('click', getCity);
-    //getCity() inserts user selected city into the fetch url and displays the weather info for current weather and five day forcast
-
     function getCity(event) {
     const city = searched.value
     cityName.innerHTML = "Here's current weather in " + city + ':';
     const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=a3f3af924ab3526ef547b4a14eefdfc8&units=imperial`
     event.preventDefault();
-
 
     fetch(requestUrl)
         .then(function (response) {
@@ -49,21 +34,12 @@ function getDays(){
 
             //Getting the min and max values for each day
             for (i = 0; i < 5; i++) {
-                document.getElementById('day' + (i + 1) + 'Min').innerHTML = 'Min: ' + Number(data.list[i].main.temp_min ).toFixed(0) + '°';
+                document.getElementById("day" + (i + 1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min ).toFixed(1) + "°";
+                //Number(1.3450001).toFixed(2); // 1.35
             };
 
             for (i = 0; i < 5; i++) {
-                document.getElementById('day' + (i + 1) + 'Max').innerHTML = 'Max: ' + Number(data.list[i].main.temp_max ).toFixed(0) + '°';
-                console.log(data.list[i])
-            };
-            //itterates over the currentData array, writes a p in the html and sets each with a unique id.
-            for (i = 0; i < 3; i++) {
-                const currentData = ['temp', 'wind', 'humidity'];
-                const p = document.createElement('p');
-                p.setAttribute('id', currentData[i]);
-                document.getElementById('current-weather').appendChild(p)
-                document.getElementById(currentData[i]).innerHTML = currentData[i]
-                //document.getElementById('day' + (i + 1) + 'Max').innerHTML = 'Max: ' + Number(data.list[i].main.temp_max ).toFixed(2) + '°';
+                document.getElementById("day" + (i + 1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max ).toFixed(2) + "°";
             };
 
             document.getElementById('temp').insertAdjacentText('afterend', data.list[1].main.temp)
